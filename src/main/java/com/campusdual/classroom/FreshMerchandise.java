7d0589e29610815866df5169e92294cebdf36890
eluiui <jesusmiguez@gmail.com>
2024-09-25 13:38:09 +0200

    Completado

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -5,4 +5,34 @@ import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    private Date expirationDate;
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String getSpecificData() {
+        String formattedDate = getFormattedDate(expirationDate);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Localización: ").append(getLocation()).append("\n");
+        sb.append("Fecha de caducidad: ").append(formattedDate);
+
+        return sb.toString();
+    }
+
+    public String getFormattedDate(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return dateFormat.format(date);
+    }
+
+    public void printSpecificData() {
+        System.out.println(getSpecificData());
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
 }
